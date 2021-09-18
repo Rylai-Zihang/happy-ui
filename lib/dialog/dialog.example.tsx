54874e8f9261d85f5c03d2bd3b9185e676dec26e
Zihang He <rylaihzh@gmail.com>
2021-09-18 11:17:19 +0800

    complete confirm and modal types

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
-import Dialog from "./dialog"
+import Dialog, { alert, confirm, modal } from "./dialog"
 
 export default function() {
   const [ visible, setVisible ] = useState(false)
+  const openModal = () => {
+    const closeModal = modal(<div><h1>modal</h1><button onClick={() => closeModal()}>关闭</button></div>)
+  }
   return (
     <div>
       <button onClick={() => { setVisible(!visible) }}>visible</button>
       <Dialog visible={visible} onClose={() => { setVisible(false) }}>
-        hahahaha
+        我是一个dialog
       </Dialog>
-      <div style={{ zIndex: 9999 }}>zIndex:9999</div>
+      <div>
+        <button onClick={() => {alert("1")}}>test alert</button>
+        <button onClick={() => {confirm("你要吃饭吗", () => {console.log("吃")}, () => {console.log("不吃")})}}>
+          test confirm
+        </button>
+        <button onClick={openModal}>test modal</button>
+      </div>
     </div>
   )
 }
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,15 +1,15 @@
 import Icon from "../icon/icon";
-import React, { Fragment } from "react";
+import React, { Fragment, MouseEventHandler, ReactFragment, ReactNode } from "react";
 import "./dialog.scss"
 import prefix from "../helpers/prefix"
 import ReactDOM from "react-dom";
 
 interface Props {
   visible: boolean,
-  onOK?: React.MouseEventHandler | null,
-  onClose: React.MouseEventHandler,
+  onOK?: MouseEventHandler|null,
+  onClose: MouseEventHandler,
   title?: String,
-  footer?: React.ReactNode | null,
+  footer?: Array<React.ReactElement>|null,
   okText?: String
   cancelText?: String,
   maskClosable?: boolean
@@ -31,6 +31,15 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
     }
   }
 
+  let buttons:Array<React.ReactElement> = []
+  // 不传或者传[]，都使用默认值
+  if(props.footer?.length === 0) {
+    buttons = [
+      <button onClick={onClickOK}>{props.okText}</button>,
+      <button onClick={props.onClose}>{props.cancelText}</button>
+    ]
+  }
+
   const returnElement = (
     props.visible ?
       <Fragment>
@@ -43,17 +52,13 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
             {props.title}
           </header>
           <main className={dialogPrefix("main")}>{props.children}</main>
-          <footer className={dialogPrefix("footer")}>
-            {props.footer?
-              props.footer:
-              (
-                <Fragment>
-                  <button onClick={onClickOK}>{props.okText}</button>
-                  <button onClick={props.onClose}>{props.cancelText}</button>
-                </Fragment>
-              )
-            }
-          </footer>
+          {buttons && buttons.length > 0 &&
+              <footer className={dialogPrefix('footer')}>
+                {buttons && buttons.map((button, index) =>
+                  React.cloneElement(button, {key: index})
+                )}
+              </footer>
+          }
         </div>
       </Fragment> :
       null
@@ -66,11 +71,52 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
 
 Dialog.defaultProps = {
   title: "标题",
-  onOK: null,
   maskClosable: true,
   okText: "确定",
   cancelText: "取消",
-  footer: null
+  footer: []
 }
+
+const type = function(content, footer?, confirm?: () => void, cancel?: () => void, ) {
+  const onClose = () => {
+    ReactDOM.render(React.cloneElement(component, { visible: false}), div)
+    // 卸载div中的组件
+    ReactDOM.unmountComponentAtNode(div)
+    // 删除div
+    div.remove()
+  }
+  const onConfirm = () => {
+    onClose()
+    confirm && confirm()
+  }
+  const onCancel = () => {
+    onClose()
+    cancel && cancel()
+  }
+  const component = (
+  <Dialog visible={true} footer={footer} onOK={(confirm? onConfirm : null)} onClose={onCancel}>
+    {content}
+  </Dialog>)
+  const div = document.createElement("div")
+  document.body.append(div)
+  ReactDOM.render(component, div)
+  return onClose
+}
+
+const alert = (content: string) => {
+  type(content, null)
+}
+
+const confirm = (content: string, confirm?: () => void, cancel?: () => void) => {
+  // todo
+  type(content, [], confirm, cancel)
+}
+
+const modal = (content: ReactNode|ReactFragment) => {
+  return type(content)
+}
+
+
+export { alert, confirm, modal }
 
 export default  Dialog
