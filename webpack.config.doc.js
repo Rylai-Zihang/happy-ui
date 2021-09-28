44e97f24f5a03610ea6d2cc59515716a8bb08a48
Zihang He <rylaihzh@gmail.com>
2021-09-28 15:51:06 +0800

    add files for docs

---
+++ webpack.config.doc.js
@@ -0,0 +1,18 @@
+const base = require('./webpack.config')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = Object.assign({}, base, {
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'doc'),
+  },
+  entry: {
+    example: './example.tsx',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'example.html',
+      filename: 'index.html'
+    })
+  ]
+})
